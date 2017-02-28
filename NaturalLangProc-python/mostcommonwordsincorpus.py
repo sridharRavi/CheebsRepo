7c0b9116d097426b3abff8abb0c87c1dabcf3b12
Sridhar Ravichandran <sridhar.chandran97@gmail.com>
2017-02-28 20:14:56 +0530

    Update mostcommonwordsincorpus.py

--- NaturalLangProc-python/mostcommonwordsincorpus.py
+++ NaturalLangProc-python/mostcommonwordsincorpus.py
@@ -1,5 +1,5 @@
 import nltk
-from nltk.corpus import movie_reviews #ysing the movie review corpus in nltk package.
+from nltk.corpus import movie_reviews #using the movie review corpus in nltk package.
 import random
 
 #documents=[(list(movie_reviews.words(fileid)),category) for category in movie_reviews.categories()
@@ -8,6 +8,7 @@ import random
 all_words=[];
 for w in movie_reviews.words():
 	all_words.append(w.lower())
-all_words=nltk.FreqDist(all_words);
+#all_words contain all the words within the movie_reviews folder (abt 2000 reviews) 
+all_words=nltk.FreqDist(all_words);#uses frequency disribution to map commonly occuring words
 #we will print the most commonly used 100 words
 print(all_words.most_common(100))
